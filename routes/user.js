dd02c48ad2a2fcd09ca9dc8de953404ab895fc48
Andrew Haddock <ahaddock523@gmail.com>
2016-12-01 17:28:34 -0700

    added user register and login routes, included logout

--- routes/user.js
+++ routes/user.js
@@ -13,42 +13,64 @@ router.get('/login', function(request, response) {
     }
 });
 
-// router.post('/login', function(request, response) {
-//     User.findOne(
-//         {
-//             username: request.body.username,
-//             password: request.body.password
-//         },
-//         {},
-//         function(error, result) {
-//             if (error) {
-//                 console.error('*** ERROR: Problem finding the user.');
-//                 console.error(error);
-//                 response.send('There was an error with the page.');
-//             }
-//             else if (!result) {
-//                 request.flash('error', 'Your username or password is not correct');
-//                 response.redirect('/user/login');
-//
-//                 console.log('***Test: ', request);
-//             }
-//             else {
-//                 console.log('This is the found user: ', result);
-//
-//                 request.session.user = {
-//                     username: result.username,
-//                     email: result.email
-//                 }
-//                 console.log('This is the session data: ', request.session);
-//
-//                 response.redirect ('/search');
-//             }
-//         }
-//     );
-// });
-//
+router.post('/login', function(request, response) {
+    User.findOne(
+        {
+            username: request.body.username,
+            password: request.body.password
+        },
+        {},
+        function(error, result) {
+            if (error) {
+                console.error('*** ERROR: Problem finding the user.');
+                console.error(error);
+                response.send('There was an error with the page.');
+            }
+            else if (!result) {
+                request.flash('error', 'Your username or password is not correct');
+                response.redirect('/user/login');
+
+                console.log('***Test: ', request);
+            }
+            else {
+                console.log('This is the found user: ', result);
+
+                request.session.user = {
+                    username: result.username,
+                    email: result.email
+                }
+                console.log('This is the session data: ', request.session);
+
+                response.redirect ('/search');
+            }
+        }
+    );
+});
+
 router.get('/register', function(request, response) {
     response.render ('register');
+});
+
+router.post('/register', function(request, response) {
+        var newUser = User(request.body);
+
+        newUser.save (function(error) {
+            if(error) {
+                var errorMessage = 'Unable to save the user to the database.'
+                console.error('***ERROR: ', errorMessage);
+                console.error(error);
+                response.send(errorMessage);
+            }
+            else {
+                request.flash('success', 'Registered Successfully, please login.')
+                response.redirect('/user/login')
+            }
+        });
+});
+
+router.get('/logout', function(request, response) {
+    request.session.destroy();
+    response.redirect('/user/login');
 });
 
 module.exports = router;
